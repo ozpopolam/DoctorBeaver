dbd35c4be647444cc3b20cfa9047d461a474fe8b
Anastasia Stepanova-Kolupakhina <Asia@clientlan-0-101.mlzone.ru>
2016-02-06 23:45:58 +0300

    Initial Commit

---
+++ DoctorBeaverTests/DoctorBeaverTests.swift
@@ -0,0 +1,36 @@
+//
+//  DoctorBeaverTests.swift
+//  DoctorBeaverTests
+//
+//  Created by Anastasia Stepanova-Kolupakhina on 06.02.16.
+//  Copyright © 2016 Anastasia Stepanova-Kolupakhina. All rights reserved.
+//
+
+import XCTest
+@testable import DoctorBeaver
+
+class DoctorBeaverTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
